49ad542d5d91427f19d80522a87e06159aa92416
Mingyong Cai <Mingyong Cai>
2020-02-23 14:25:47 -0500

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Wei Nie on 2/23/20.
+//  Copyright © 2020 Terrycai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
